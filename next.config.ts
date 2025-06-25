573b977912c4159abdd09b8a2eac20499a52f73b
edwini69 <darkbroks@gmail.com>
2025-06-25 11:26:54 -0400

    dp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/patientView_registroMedico', // reemplaza con el nombre real
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
